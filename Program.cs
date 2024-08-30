eea7e809b5a863c5dbd0f460b8a2cced4c990a23
souryzetma <91234492+souryzetma@users.noreply.github.com>
2024-08-30 19:56:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LopCircleVaCylinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.setColor("Red");
+            circle.setR(5);
+            Console.WriteLine(circle.ToString());
+            Cylinder cylinder = new Cylinder();
+            cylinder.setColor("Blue");
+            cylinder.setR(3);
+            cylinder.setHeight(10);
+            cylinder.getArea();
+            Console.WriteLine(cylinder.ToString());
+        }
+    }
+}
